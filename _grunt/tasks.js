0e137ae53570d84af4086e821de2c186c9b6b320
andrewmalone <andrew@andrewmalone.com>
2014-12-26 17:10:55 -0500

    Fix task name for validation

--- _grunt/tasks.js
+++ _grunt/tasks.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 	// add sass, autoprefix, concat, etc. to this tasks
 	grunt.registerTask('build', ['css', 'js', 'shell:jekyllPublish']);
 	
-	grunt.registerTask('build-dev', ['css', 'csslint', 'js', 'jshint', 'shell:jekyllPublish', 'validate']);
+	grunt.registerTask('build-dev', ['css', 'csslint', 'js', 'jshint', 'shell:jekyllPublish', 'validation']);
 	
 	grunt.registerTask('dev', ['build-dev', 'connect', 'watch']);
 };
